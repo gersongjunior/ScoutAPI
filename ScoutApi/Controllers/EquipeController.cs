cec68d32f0911f5c8a0d5e35ea8b43fc2b07a8b0
Gerson Goncalves Junior <gerson.junior1986@gmail.com>
2022-10-18 23:47:47 -0300

    Inicio projeto

---
+++ ScoutApi/Controllers/EquipeController.cs
@@ -0,0 +1,41 @@
+﻿using ScoutApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace ScoutApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EquipeController : ControllerBase
+    {
+        private static List<Equipe> equipes = new List<Equipe>();
+        private static int id = 1;
+
+        [HttpPost]
+        public IActionResult AdicionaEquipe([FromBody] Equipe equipe)
+        {
+            equipe.Id = id++;
+            equipes.Add(equipe);
+            return CreatedAtAction(nameof(RecuperaEquipesPorId), new { Id = equipe.Id }, equipe);
+            
+        }
+
+        [HttpGet]
+        public IActionResult RecuperaEquipes()
+        {
+            return Ok(equipes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaEquipesPorId(int id)
+        {
+            Equipe equipe = equipes.FirstOrDefault(equipe => equipe.Id == id);
+            if(equipe != null)
+            {
+                return Ok(equipe);
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ ScoutApi/Models/Equipe.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ScoutApi.Models
+{
+    public class Equipe
+    {
+        public int Id { get;  set; }
+        [Required(ErrorMessage = "O campo Nome é obrigatório")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "O campo Cidade é obrigatório")]
+        public string Cidade { get; set; }
+        [Required(ErrorMessage = "O campo Estado é obrigatório")]
+        public string Estado { get; set; }
+        public string Modalidade { get; set; }
+        
+    }
+}
